936fa6863baaee15f3d307e6c03198698fb43c15
Klaus Ondrag <klaus.ondrag@outlook.com>
2019-12-01 14:08:32 -0800

    Add reading argument

--- 2019/01/src/main.rs
+++ 2019/01/src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    println!("In file {}", filename);
     println!("Hello, world!");
 }
