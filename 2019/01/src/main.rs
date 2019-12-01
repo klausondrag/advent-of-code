5e85e660589922c979b6e07c52ac837a063d092c
Klaus Ondrag <klaus.ondrag@outlook.com>
2019-12-01 14:22:24 -0800

    Add reading file line by line

--- 2019/01/src/main.rs
+++ 2019/01/src/main.rs
@@ -1,8 +1,15 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     println!("In file {}", filename);
-    println!("Hello, world!");
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
