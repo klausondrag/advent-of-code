3974aa38106fb5d18fe229634f359d053535d29c
Klaus Ondrag <klaus.ondrag@outlook.com>
2019-12-01 15:45:17 -0800

    Add speed tests

--- 2019/01/src/main.rs
+++ 2019/01/src/main.rs
@@ -1,6 +1,10 @@
+#[cfg(test)] #[macro_use] extern crate cached;
+#[cfg(test)] #[macro_use] extern crate timeit;
+
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -22,11 +26,13 @@ fn main() {
     println!("Part2: Total fuel requirement: {}", sum_part2);
 }
 
-pub fn solve_part1(input: i32) -> i32 {
+fn solve_part1(input: i32) -> i32 {
     (input / 3) - 2
 }
 
-pub fn solve_part2(input: i32) -> i32 {
+fn solve_part2(input: i32) -> i32 {
+    // for fibonacchi, caching requires 1/3 of the time
+    // for this problem, caching requires 3x the time :(
     let required_fuel = (input / 3) - 2;
     if required_fuel <= 0 {
         0
@@ -35,6 +41,7 @@ pub fn solve_part2(input: i32) -> i32 {
         required_fuel + fuel_for_fuel
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -54,5 +61,48 @@ mod tests {
         let solutions = vec![2, 2, 966, 50346];
         let outputs: Vec<i32> = inputs.into_iter().map(solve_part2).collect();
         assert_eq!(solutions, outputs);
+    }
+
+    #[test]
+    #[ignore]
+    fn speed_naive() {
+        fn solve_part2_naive(input: i32) -> i32 {
+            let required_fuel = (input / 3) - 2;
+            if required_fuel <= 0 {
+                0
+            } else {
+                let fuel_for_fuel = solve_part2_naive(required_fuel);
+                required_fuel + fuel_for_fuel
+            }
+        }
+        let n_loops = 1_000_000;
+        let naive_time = timeit_loops!(n_loops, {
+            let inputs = vec![12, 14, 1969, 100756];
+            let _outputs: Vec<i32> = inputs.into_iter().map(solve_part2_naive).collect();
+        });
+        println!("Time for {} loops naive : {}", n_loops, naive_time);
+    }
+
+    #[test]
+    #[ignore]
+    fn speed_cached() {
+        cached!{
+            PART2;
+            fn solve_part2_cached(input: i32) -> i32 = {
+                let required_fuel = (input / 3) - 2;
+                if required_fuel <= 0 {
+                    0
+                } else {
+                    let fuel_for_fuel = solve_part2_cached(required_fuel);
+                    required_fuel + fuel_for_fuel
+                }
+            }
+        }
+        let n_loops = 1_000_000;
+        let time = timeit_loops!(n_loops, {
+            let inputs = vec![12, 14, 1969, 100756];
+            let _outputs: Vec<i32> = inputs.into_iter().map(solve_part2_cached).collect();
+        });
+        println!("Time for {} loops cached: {}", n_loops, time);
     }
 }
