3fec2cdbfd2763b55d62299a91e6e75908f34361
Klaus Ondrag <klaus.ondrag@outlook.com>
2020-12-15 16:46:47 +0100

    Add solution for day 15

---
+++ 2020/src/day_15_part_2.rs
@@ -0,0 +1,71 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub(crate) fn process(input_filename: &str) {
+    let file = File::open(input_filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut input_numbers = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let input_number = line.parse::<usize>().unwrap();
+        input_numbers.push(input_number);
+    }
+
+    const NTH_SPOKEN_NUMBER: usize = 30000000;
+    let solution = solve(input_numbers, NTH_SPOKEN_NUMBER);
+    println!("The {}th spoken number is: {}", NTH_SPOKEN_NUMBER, solution);
+}
+
+fn solve(input_numbers: Vec<usize>, nth_spoken_number: usize) -> usize {
+    let mut number_to_turn: HashMap<usize, usize> = HashMap::new();
+
+    for (index, number) in input_numbers
+        .iter()
+        .enumerate()
+        .take(input_numbers.len() - 1)
+    {
+        // turns start with 1
+        number_to_turn.insert(*number, index + 1);
+    }
+
+    let mut previous_turn = input_numbers.len();
+    let mut spoken_number_previous_turn = *input_numbers.get(input_numbers.len() - 1).unwrap();
+
+    while previous_turn < nth_spoken_number {
+        let spoken_number_this_turn = match number_to_turn.get(&spoken_number_previous_turn) {
+            None => 0,
+            Some(last_turn_when_spoken) => previous_turn - *last_turn_when_spoken,
+        };
+
+        number_to_turn.insert(spoken_number_previous_turn, previous_turn);
+        spoken_number_previous_turn = spoken_number_this_turn;
+        previous_turn += 1;
+    }
+
+    spoken_number_previous_turn
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn example() {
+        const NTH_SPOKEN_NUMBER: usize = 30000000;
+        let inputs: Vec<Vec<usize>> = vec![
+            vec![0, 3, 6],
+            vec![1, 3, 2],
+            vec![2, 1, 3],
+            vec![1, 2, 3],
+            vec![2, 3, 1],
+            vec![3, 2, 1],
+            vec![3, 1, 2],
+        ];
+        let solutions: Vec<usize> = vec![175594, 2578, 3544142, 261214, 6895259, 18, 362];
+        let outputs: Vec<usize> = inputs
+            .iter()
+            .map(|input_numbers| super::solve(input_numbers.to_vec(), NTH_SPOKEN_NUMBER))
+            .collect();
+        assert_eq!(solutions, outputs)
+    }
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -22,6 +22,8 @@ mod day_12_part_1;
 mod day_12_part_2;
 mod day_13_part_1;
 mod day_13_part_2;
+mod day_15_part_1;
+mod day_15_part_2;
 
 use std::collections::HashMap;
 use std::path::Path;
@@ -64,6 +66,8 @@ fn main() {
     programs.insert(String::from("12-2"), day_12_part_2::process);
     programs.insert(String::from("13-1"), day_13_part_1::process);
     programs.insert(String::from("13-2"), day_13_part_2::process);
+    programs.insert(String::from("15-1"), day_15_part_1::process);
+    programs.insert(String::from("15-2"), day_15_part_2::process);
 
     let key = day.to_owned() + "-" + part;
     let program = programs.get(&key);
